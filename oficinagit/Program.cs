02fecfec75724100405db03289b9cd60f0fb3b8a
Lucas Oliveira Nascimento <lucasolivveira297@gmail.com>
2023-10-05 15:08:41 -0400

    Paradoxo do eduardo costa

---
+++ oficinagit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Transactions;
+
+class gitcurso
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite outro número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double resultado = num + num2;
+        Console.WriteLine($"A soma dos números é {resultado}");
+        Console.ReadKey();
+    }
+}
