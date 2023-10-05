987b6f7a0d58ad34c1e8a23db095f6a1a1f2a41d
Lucas Oliveira Nascimento <lucasolivveira297@gmail.com>
2023-10-05 15:47:44 -0400

    oficinagit

--- oficinagit/Program.cs
+++ oficinagit/Program.cs
@@ -5,12 +5,14 @@ class gitcurso
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Digite um número: ");
+        Console.Write("Digite um número: ");
         double num = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine("Digite outro número: ");
+        Console.Write("Digite outro número: ");
         double num2 = Convert.ToDouble(Console.ReadLine());
         double resultado = num + num2;
         Console.WriteLine($"A soma dos números é {resultado}");
+
+        if (num2 > num) Console.WriteLine($"O numero {num2} é maior");
         Console.ReadKey();
     }
 }
